9548e06fb89e8ec55a6820ce6422ad57c2077049
brizism <montielbriseida@gmail.com>
2018-05-03 19:10:27 -0400

    destructure user's id

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import CreateScreen from './CreateScreen';
 
 class App extends Component {
+  state = {
+    resumeCopy: {}
+  }
+
+
   render() {
+    const { params } = this.props.match;
     return (
       <div>
-        <h1>Hola</h1>
+        <h1>Hi {params.resumeId}!</h1>
+        <CreateScreen />
       </div>
     );
   }
