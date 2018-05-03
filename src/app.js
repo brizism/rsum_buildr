7efac38ac97dfa81b770553e3592b73b82dd86ee
brizism <montielbriseida@gmail.com>
2018-05-03 16:13:10 -0400

    rename entry point to index.js

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'normalize.css/normalize.css';
-import 'ress/ress.css';
-import './styles/styles.scss'
-
-ReactDOM.render(<App />, document.getElementById('app'));
